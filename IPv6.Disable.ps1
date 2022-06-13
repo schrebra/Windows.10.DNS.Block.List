8ff390f43bf980442fbc1719a1a05d021e935dbb
schrebra <34587652+schrebra@users.noreply.github.com>
2022-06-12 19:31:10 -0700

    Add files via upload

---
+++ IPv6.Disable.ps1
@@ -0,0 +1,14 @@
+﻿
+write-host ""
+write-host "Disabling IPv6 ..." -ForegroundColor green
+write-host ""
+Disable-NetAdapterBinding -Name '*' -ComponentID 'ms_tcpip6'
+
+write-host "============IPv6 Status============" -ForegroundColor Magenta
+get-NetAdapterBinding -Name '*' -ComponentID 'ms_tcpip6' | format-table -AutoSize -Property Name, Enabled 
+
+
+
+
+
+
---
+++ IPv6.Enable.ps1
@@ -0,0 +1,15 @@
+﻿
+write-host ""
+write-host "Enabling IPv6 ..." -ForegroundColor green
+write-host ""
+enable-NetAdapterBinding -Name '*' -ComponentID 'ms_tcpip6'
+
+write-host "============IPv6 Status============" -ForegroundColor Magenta
+get-NetAdapterBinding -Name '*' -ComponentID 'ms_tcpip6' | format-table -AutoSize -Property Name, Enabled 
+
+
+
+
+
+
+
